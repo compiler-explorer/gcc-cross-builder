cecde4083c23e3627ddb4b909f27cff34d5601c3
Marc Poulhiès <dkm@kataplop.net>
2023-01-13 21:42:11 +0100

    fix: local_build.sh not handling trunk correctly
    When building "trunk", add missing date in output file name.
    
    Signed-off-by: Marc Poulhiès <dkm@kataplop.net>

--- local_build.sh
+++ local_build.sh
@@ -10,12 +10,17 @@ RET=0
 
 mkdir -p $OUTDIR
 
-CID=$(docker create -ti --name dummy gcc-cross ./build.sh "$ARCH" "$VERSION" /opt/ nope)
+if [ "$VERSION" = "trunk" ]; then
+    OUTPUT_VERSION_FILE="${VERSION}-$(date +%Y%m%d)"
+else
+    OUTPUT_VERSION_FILE="${VERSION}"
+fi
 
+CID=$(docker create -ti --name dummy gcc-cross ./build.sh "$ARCH" "$VERSION" /opt/ nope)
 
 if docker start -a "$CID";
 then
-   docker cp "dummy:/opt/$ARCH-gcc-$VERSION.tar.xz" "$OUTDIR"/
+   docker cp "dummy:/opt/$ARCH-gcc-${OUTPUT_VERSION_FILE}.tar.xz" "$OUTDIR"/
    echo "$ARCH successful"
 else
     echo "$ARCH not successful"
