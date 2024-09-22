2befa84959278b148f9fa33822507e9dff6fa0c5
Sam James <sam@gentoo.org>
2024-09-22 04:29:29 +0100

    local_build.sh: support podman too if docker isn't available
    They have a compatible CLI.

--- local_build.sh
+++ local_build.sh
@@ -10,21 +10,26 @@ RET=0
 
 mkdir -p $OUTDIR
 
+DOCKER=docker
+if ! type -P ${DOCKER} 2>/dev/null ; then
+	DOCKER=podman
+fi
+
 if [ "$VERSION" = "trunk" ]; then
     OUTPUT_VERSION_FILE="${VERSION}-$(date +%Y%m%d)"
 else
     OUTPUT_VERSION_FILE="${VERSION}"
 fi
 
-CID=$(docker create -ti --name dummy gcc-cross ./build.sh "$ARCH" "$VERSION" /opt/ nope)
+CID=$(${DOCKER} create -ti --name dummy gcc-cross ./build.sh "$ARCH" "$VERSION" /opt/ nope)
 
-if docker start -a "$CID";
+if ${DOCKER} start -a "$CID";
 then
-   docker cp "dummy:/opt/$ARCH-gcc-${OUTPUT_VERSION_FILE}.tar.xz" "$OUTDIR"/
+   ${DOCKER} cp "dummy:/opt/$ARCH-gcc-${OUTPUT_VERSION_FILE}.tar.xz" "$OUTDIR"/
    echo "$ARCH SUCCESS"
 else
     echo "$ARCH FAILED"
     RET=1
 fi
-docker rm -f dummy
+${DOCKER} rm -f dummy
 exit "$RET"
